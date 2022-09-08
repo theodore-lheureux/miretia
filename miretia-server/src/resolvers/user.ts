db4331534c8d20a48a49859823d53a073bf9ea58
Théodore L’Heureux <theodorelheureux@gmail.com>
2022-09-08 14:31:38 +0000

    Add user query to get user by id

--- miretia-server/src/resolvers/user.ts
+++ miretia-server/src/resolvers/user.ts
@@ -1,12 +1,12 @@
 import "reflect-metadata";
 import {
-	Resolver,
-	Query,
-	Mutation,
-	Arg,
-	Ctx,
-	ObjectType,
-	Field,
+  Resolver,
+  Query,
+  Mutation,
+  Arg,
+  Ctx,
+  ObjectType,
+  Field,
 } from "type-graphql";
 import { User } from "../entities/User";
 import { Context } from "../context";
@@ -16,19 +16,19 @@ import argon2 from "argon2";
 
 @ObjectType()
 class FieldError {
-	@Field()
-	field: string;
-	@Field()
-	message: string;
+  @Field()
+  field: string;
+  @Field()
+  message: string;
 }
 
 @ObjectType()
 class UserResponse {
-	@Field(() => [FieldError], { nullable: true })
-	errors?: FieldError[];
+  @Field(() => [FieldError], { nullable: true })
+  errors?: FieldError[];
 
-	@Field(() => User, { nullable: true })
-	user?: User;
+  @Field(() => User, { nullable: true })
+  user?: User;
 }
 
 // @ObjectType()
@@ -42,46 +42,72 @@ class UserResponse {
 
 @Resolver(UserResponse)
 export class UserResolver {
-	@Mutation(() => UserResponse)
-	async register(
-		@Arg("data") options: UsernamePasswordInput,
-		@Ctx() ctx: Context
-	): Promise<UserResponse> {
-		const errors = validateRegister(options);
+  @Query(() => UserResponse)
+  async user(
+    @Arg("id") id: number,
+    @Ctx() ctx: Context
+  ): Promise<UserResponse> {
+    var user: User | null = await ctx.prisma.user.findUnique({
+      where: {
+        id,
+      },
+    });
 
-		if (errors) {
-			return { errors };
-		}
-
-		const hashedPassword = await argon2.hash(options.password);
-		let user: User;
-
-		try {
-			user = await ctx.prisma.user.create({
-				data: {
-					username: options.username,
-					email: options.email,
-					password: hashedPassword,
-				},
-			});
-		} catch (error) {
-			return {
-				errors: [
-					{
-						field: "username",
-						message: "Username/email already exists.",
-					},
-				],
-			};
-		}
-
+    if (user)
 		return {
-			user: user,
-		};
-	}
+			user
+		}
+	else
+		return {
+			errors: [
+				{
+					field: "id",
+					message: "No user with corresponding ID."
+				}
+			]
+		}
+  }
 
-	@Query(() => [User])
-	async allUsers(@Ctx() ctx: Context) {
-		return ctx.prisma.user.findMany();
-	}
+  @Mutation(() => UserResponse)
+  async register(
+    @Arg("options") options: UsernamePasswordInput,
+    @Ctx() ctx: Context
+  ): Promise<UserResponse> {
+    const errors = validateRegister(options);
+
+    if (errors) {
+      return { errors };
+    }
+
+    const hashedPassword = await argon2.hash(options.password);
+    let user: User;
+
+    try {
+      user = await ctx.prisma.user.create({
+        data: {
+          username: options.username,
+          email: options.email,
+          password: hashedPassword,
+        },
+      });
+    } catch (error) {
+      return {
+        errors: [
+          {
+            field: "username",
+            message: "Username/email already exists.",
+          },
+        ],
+      };
+    }
+
+    return {
+      user: user,
+    };
+  }
+
+  @Query(() => [User])
+  async allUsers(@Ctx() ctx: Context) {
+    return ctx.prisma.user.findMany();
+  }
 }
